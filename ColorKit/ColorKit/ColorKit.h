380e15a94a095fac2e86c4370854c058f873cccd
Boris Emorine <boris.emorine@gmail.com>
2020-02-24 22:08:03 +0100

    Add XCode project

---
+++ ColorKit/ColorKit/ColorKit.h
@@ -0,0 +1,19 @@
+//
+//  ColorKit.h
+//  ColorKit
+//
+//  Created by Boris Emorine on 2/24/20.
+//  Copyright © 2020 BorisEmorine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ColorKit.
+FOUNDATION_EXPORT double ColorKitVersionNumber;
+
+//! Project version string for ColorKit.
+FOUNDATION_EXPORT const unsigned char ColorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorKit/PublicHeader.h>
+
+
