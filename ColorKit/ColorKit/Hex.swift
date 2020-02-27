598ae94624c8248119474a64682224ac34c61fa3
Boris Emorine <boris.emorine@gmail.com>
2020-02-27 22:08:24 +0100

    Add Hex features

---
+++ ColorKit/ColorKit/Hex.swift
@@ -0,0 +1,41 @@
+//
+//  Hex.swift
+//  ColorKit
+//
+//  Created by Boris Emorine on 2/27/20.
+//  Copyright © 2020 BorisEmorine. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    public convenience init?(hex: String) {
+        let hexString = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        
+        var hexValue = UInt64()
+        
+        guard Scanner(string: hexString).scanHexInt64(&hexValue) else {
+            return nil
+        }
+        
+        let a, r, g, b: UInt64
+        switch hexString.count {
+        case 3: // 0xRGB
+            (a, r, g, b) = (255, (hexValue >> 8) * 17, (hexValue >> 4 & 0xF) * 17, (hexValue & 0xF) * 17)
+        case 6: // 0xRRGGBB
+            (a, r, g, b) = (255, hexValue >> 16, hexValue >> 8 & 0xFF, hexValue & 0xFF)
+        case 8: // 0xRRGGBBAA
+            (a, r, g, b) = (hexValue >> 24, hexValue >> 16 & 0xFF, hexValue >> 8 & 0xFF, hexValue & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+    
+    public var hex: String {
+        let rgb: Int = (Int)(self.red * 255) << 16 | (Int)(self.green * 255) << 8 | (Int)(self.blue * 255) << 0
+        return String(format: "#%06x", rgb)
+    }
+    
+}
---
+++ ColorKit/ColorKitTests/HexTests.swift
@@ -0,0 +1,75 @@
+//
+//  HexTests.swift
+//  ColorKitTests
+//
+//  Created by Boris Emorine on 2/27/20.
+//  Copyright © 2020 BorisEmorine. All rights reserved.
+//
+
+import XCTest
+@testable import ColorKit
+
+class HexTests: XCTestCase {
+    
+    private let blackHex = "#000000"
+    private let whiteHex = "#ffffff"
+    private let redHex = "#ff0000"
+    private let darkGreen = "#32a852"
+
+    // Init
+    
+    func testInitBlack() {
+        let color = UIColor(hex: blackHex)!
+        XCTAssertEqual(color.red, 0)
+        XCTAssertEqual(color.green, 0)
+        XCTAssertEqual(color.blue, 0)
+        XCTAssertEqual(color.alpha, 1)
+    }
+    
+    func testInitWhite() {
+        let color = UIColor(hex: whiteHex)!
+        XCTAssertEqual(color.red, 1)
+        XCTAssertEqual(color.green, 1)
+        XCTAssertEqual(color.blue, 1)
+        XCTAssertEqual(color.alpha, 1)
+    }
+    
+    func testInitRed() {
+        let color = UIColor(hex: redHex)!
+        XCTAssertEqual(color.red, 255.0 / 255.0)
+        XCTAssertEqual(color.green, 0.0 / 255.0)
+        XCTAssertEqual(color.blue, 0.0 / 255.0)
+        XCTAssertEqual(color.alpha, 1)
+    }
+    
+    func testInitDarkGreen() {
+        let color = UIColor(hex: darkGreen)!
+        XCTAssertEqual(color.red, 50.0 / 255.0)
+        XCTAssertEqual(color.green, 168.0 / 255.0)
+        XCTAssertEqual(color.blue, 82.0 / 255.0)
+        XCTAssertEqual(color.alpha, 1)
+    }
+    
+    // hex
+    
+    func testHexBlack() {
+        let color = UIColor.black
+        XCTAssertEqual(color.hex, blackHex)
+    }
+    
+    func testHexWhite() {
+        let color = UIColor.white
+        XCTAssertEqual(color.hex, whiteHex)
+    }
+    
+    func testHexRed() {
+        let color = UIColor.red
+        XCTAssertEqual(color.hex, redHex)
+    }
+    
+    func testHexDarkGreen() {
+        let color = UIColor(red: 50.0 / 255.0, green: 168.0 / 255.0, blue: 82.0 / 255.0, alpha: 1.0)
+        XCTAssertEqual(color.hex, darkGreen)
+    }
+    
+}
