6e44214effb968e423d4507fb07ef199b2ab0682
Boris Emorine <boris.emorine@gmail.com>
2020-05-09 22:54:37 +0200

    Add random color generation.
    Gives an easy way to generate a random color.

---
+++ ColorKit/ColorKit/Random.swift
@@ -0,0 +1,25 @@
+//
+//  Random.swift
+//  ColorKit
+//
+//  Created by Boris Emorine on 5/5/20.
+//  Copyright © 2020 BorisEmorine. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    /// Generates a random `UIColor` instance.
+    /// - Parameters:
+    ///   - randomizeAlpha: Whether the alpha channel should also be randomized. If set to false (default), it will be set to 1.0.
+    public static func random(randomizeAlpha: Bool = false) -> UIColor {
+        let r = CGFloat.random(in: 0...255)
+        let g = CGFloat.random(in: 0...255)
+        let b = CGFloat.random(in: 0...255)
+        let a = randomizeAlpha ? 1 : CGFloat.random(in: 0...1)
+        
+        return UIColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
+    }
+    
+}
