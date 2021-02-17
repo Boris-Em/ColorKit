c7b5fa96fa5eaa7da5882ece4ca1c880079e5908
Patrik Svoboda <svob.pata@gmail.com>
2021-02-17 21:04:27 +0100

    Package.swift changes + tvOS support

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ColorKit",
+    defaultLocalization: "en",
     platforms: [
         .iOS(.v13),
+        .tvOS(.v14),
     ],
     products: [
         .library(
@@ -16,11 +18,14 @@ let package = Package(
     targets: [
         .target(
             name: "ColorKit",
-            path: "ColorKit/ColorKit"),
+            path: "ColorKit/ColorKit",
+            exclude: ["ColorKit/ColorKitTests/Resources"]
+        ),
         .testTarget(
             name: "ColorKitTests",
             dependencies: ["ColorKit"],
-            path: "ColorKit/ColorKitTests"),
-    ],
-    swiftLanguageVersions: [SwiftVersion.v5]
+            path: "ColorKit/ColorKitTests",
+            resources: [.copy("Resources")]
+        ),
+    ]
 )
